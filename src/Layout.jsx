eb1624a7bd8cdbd283cfee7334265b238d77ce47
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-11 23:53:37 +0600

    footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import Headers from './components/Headers/Headers';
+import Footer from './components/Footer';
 
 const Layout = () => {
     return (
         <div>
             <Headers></Headers>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
